b817cd506b1d8da439cc0395731ff9f19e4e31ee
adoxic <griffinmaeve@gmail.com>
2019-11-13 11:50:28 -0800

    now with add and delete

--- src/components/markdown/Tabs.js
+++ src/components/markdown/Tabs.js
@@ -2,20 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Tabs.css';
 
-const Tabs = ({ arrayTabs, handleTab }) => {
+const Tabs = ({ arrayTabs, handleTab, handleAdd, handleDelete }) => {
   const mappedTabs = arrayTabs.map((tab, index)=> {
-    return <button key={index} onClick={() => handleTab(tab)}>{tab}</button>;
+    return <div key={index} onClick={() => handleTab(tab)}>
+      <p>{tab}</p>
+      <button onClick={() => handleDelete(tab)}>X</button>
+    </div>;
   });
 
   return (
     <ul className={styles.Tabs}>
       {mappedTabs}
+      <button onClick={() => handleAdd()}>Add Tab</button>
     </ul>
   );
 };
 
 Tabs.propTypes = {
+  handleDelete: PropTypes.func,
   handleTab: PropTypes.func,
+  handleAdd: PropTypes.func,
   arrayTabs: PropTypes.array.isRequired
 };
 
