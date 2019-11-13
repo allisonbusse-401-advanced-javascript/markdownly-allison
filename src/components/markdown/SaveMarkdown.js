ceb921c67d338af98f2e1921d933d2d59f7ae23d
allisonbusse <allisonbusse@gmail.com>
2019-11-13 14:06:16 -0800

    save functionality

--- src/components/markdown/SaveMarkdown.js
+++ src/components/markdown/SaveMarkdown.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SaveMarkdown = ({ handleAdd }) => {
+const SaveMarkdown = ({ handleAdd, handleChange, tabName }) => {
   return (
-    <button onClick={() => handleAdd()}>Add Tab</button>
+    <>
+    <input value={tabName} onChange={handleChange} placeholder="Name of File"></input>
+    <button onClick={() => handleAdd(tabName)}>Add Tab</button>
+    </>
   );
 };
 
 SaveMarkdown.propTypes = {
-  handleAdd: PropTypes.func.isRequired
+  handleAdd: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  tabName: PropTypes.string.isRequired
 };
 
 export default SaveMarkdown;
